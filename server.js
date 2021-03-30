518ba673c042af49f3b98d4810b09287142d958b
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-03-30 09:58:25 -0600

    finished post and get with an error when not correctly formatted

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post('/api/animals', (req, res) => {
 
     //if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-        res.status(400).send(`The animal is nor properly formatted.`);
+        res.status(400).send(`The animal is not properly formatted.`);
     } else {
         //add animal to json file and animals array in this function
         const animal = createNewAnimal(req.body, animals);
